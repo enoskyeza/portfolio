7223e7360719c3d28a42863f547dc732f04e76fa
enoskyeza <enoskyeza@gmail.com>
2024-08-13 02:13:07 +0300

    add about to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
+import About from "@/components/about";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-between p-12">
+    <main className="flex min-h-screen flex-col p-12">
       <Hero />
+        <About />
     </main>
   );
 }
