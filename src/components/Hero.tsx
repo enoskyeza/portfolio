eba66e8e6b18091ad303d5c5d4cec296a424da2c
enoskyeza <enoskyeza@gmail.com>
2024-08-12 22:48:26 +0300

    add social icons

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 const Hero = () => {
     return (
@@ -6,10 +7,10 @@ const Hero = () => {
             <h1>Mitti Enos Kyeza</h1>
             <h3>Senior FullStack Engineer</h3>
             <p>I build functional and user-friendly software solutions</p>
-            <div className="socials">
-                <a href="#">Github</a>
-                <a href="#">LinkedIn</a>
-                <a href="#">Instagram</a>
+            <div className="socials flex">
+                <a href="#"><FaGithub height={20}/></a>
+                <a href="#"><FaLinkedin height={20}/></a>
+                <a href="#"><FaInstagram height={20}/></a>
             </div>
         </div>
     )
