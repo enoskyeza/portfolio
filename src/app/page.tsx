ebb00a90a66e70ab014f402f28af52ce8d818ddd
enoskyeza <enoskyeza@gmail.com>
2024-08-13 02:48:37 +0300

    format code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import About from "@/components/about";
+import Experience from "@/components/experience";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col p-12">
-      <Hero />
-        <About />
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col p-12">
+            <Hero/>
+            <About/>
+            <Experience/>
+        </main>
+    );
 }
