fe760ec56125903427b9d07ae078d479dac3805d
enoskyeza <enoskyeza@gmail.com>
2024-08-13 02:47:56 +0300

    create experience section

---
+++ src/components/experience.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+const Experience = () => {
+    return (
+        <section className="section-padding" id="experience-section">
+            <h2 className="section-title">Experience</h2>
+            <div>
+                <div className="xp-date">
+                    <p>2024-PRESENT</p>
+                </div>
+                <div>
+                    <h4 className="xp-title">Senior Frontend Engineer, Accessibility · Klaviyo</h4>
+            <p className="xp-body">
+                As a full-stack software developer, I am passionate about creating beautiful and performant products
+                with delightful user experiences. I have a bachelors degree from Mbarara University of Science and
+                Technology, and I have acquired multiple certifications in Git, GitHub, Python Data Structures and
+                Algorithms, and Software Development Life Cycle from LinkedIn.
+            </p>
+                    <div>
+                        <div className="xp-skill">Javascript</div>
+                        <div className="xp-skill">Typescript</div>
+                        <div className="xp-skill">React</div>
+                        <div className="xp-skill">Tailwind</div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Experience
