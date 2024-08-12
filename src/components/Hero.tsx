188d44cb9c7c75ae7c63f508d41327d797788cdf
enoskyeza <enoskyeza@gmail.com>
2024-08-12 22:42:13 +0300

    creat hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Hero = () => {
+    return (
+        <div>
+            <h1>Mitti Enos Kyeza</h1>
+            <h3>Senior FullStack Engineer</h3>
+            <p>I build functional and user-friendly software solutions</p>
+            <div className="socials">
+                <a href="#">Github</a>
+                <a href="#">LinkedIn</a>
+                <a href="#">Instagram</a>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
