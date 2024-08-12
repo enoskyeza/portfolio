0095bc6e8a372675d1025fb02f93e0adb3bf81aa
enoskyeza <enoskyeza@gmail.com>
2024-08-12 23:42:21 +0300

    add hero styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col justify-between p-12">
       <Hero />
     </main>
   );
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,14 +3,28 @@ import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 const Hero = () => {
     return (
-        <div>
-            <h1>Mitti Enos Kyeza</h1>
-            <h3>Senior FullStack Engineer</h3>
-            <p>I build functional and user-friendly software solutions</p>
-            <div className="socials flex">
-                <a href="#"><FaGithub height={20}/></a>
-                <a href="#"><FaLinkedin height={20}/></a>
-                <a href="#"><FaInstagram height={20}/></a>
+        <div className="py-8">
+            <h1 className="text-5xl font-bold pb-4">Mitti Enos Kyeza</h1>
+            <h3 className="text-xl font-medium pb-4">Senior FullStack Engineer</h3>
+            <p className="font-extralight text-gray-400"
+               style={{ width: "280px", lineHeight:"1.6"}}>
+                I build software solutions that deliver exceptional user experiences.</p>
+            <div className="socials flex gap-5 pt-8">
+                <a href="https://github.com/enoskyeza" target="_blank">
+                    <FaGithub
+                        className="text-gray-400 hover:text-gray-200 transition-colors duration-300 ease-in-out"
+                        size={25}/>
+                </a>
+                <a href="https://www.linkedin.com/in/enos-kyeza-a48447131/" target="_blank">
+                    <FaLinkedin
+                        className="text-gray-400 hover:text-gray-200 transition-colors duration-300 ease-in-out"
+                        size={25}/>
+                </a>
+                <a href="https://www.instagram.com/enoskyeza/" target="_blank">
+                    <FaInstagram
+                        className="text-gray-400 hover:text-gray-200 transition-colors duration-300 ease-in-out"
+                        size={25}/>
+                </a>
             </div>
         </div>
     )
