691a89fba8f4164cb52e56a53f787e2d985705f8
enoskyeza <enoskyeza@gmail.com>
2024-08-13 03:25:15 +0300

    Ass experience section styles

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -5,23 +5,26 @@ const Experience = () => {
     return (
         <section className="section-padding" id="experience-section">
             <h2 className="section-title">Experience</h2>
-            <div>
-                <div className="xp-date">
-                    <p>2024-PRESENT</p>
+            <div className="flex gap-3">
+                <div className="w-1/4">
+                    <p className="xp-date">2024 - PRESENT</p>
                 </div>
-                <div>
+                <div className='w-3/4'>
                     <h4 className="xp-title">Senior Frontend Engineer, Accessibility · Klaviyo</h4>
-            <p className="xp-body">
-                As a full-stack software developer, I am passionate about creating beautiful and performant products
-                with delightful user experiences. I have a bachelors degree from Mbarara University of Science and
-                Technology, and I have acquired multiple certifications in Git, GitHub, Python Data Structures and
-                Algorithms, and Software Development Life Cycle from LinkedIn.
-            </p>
+                    <p className="xp-body">
+                        As a full-stack software developer, I am passionate about creating beautiful and performant
+                        products
+                        with delightful user experiences. I have a bachelors degree from Mbarara University of Science
+                        and
+                        Technology, and I have acquired multiple certifications in Git, GitHub, Python Data Structures
+                        and
+                        Algorithms, and Software Development Life Cycle from LinkedIn.
+                    </p>
                     <div>
-                        <div className="xp-skill">Javascript</div>
-                        <div className="xp-skill">Typescript</div>
-                        <div className="xp-skill">React</div>
-                        <div className="xp-skill">Tailwind</div>
+                        <span className="xp-skill">Javascript</span>
+                        <span className="xp-skill">Typescript</span>
+                        <span className="xp-skill">React</span>
+                        <span className="xp-skill">Tailwind</span>
                     </div>
                 </div>
             </div>
