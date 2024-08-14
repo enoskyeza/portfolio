c9cfd1cd690a8f389ac17b8c2dae60c4b871d61a
enoskyeza <enoskyeza@gmail.com>
2024-08-14 13:24:35 +0300

    create navbar

---
+++ src/components/nav.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Nav = () => {
+    return (
+        <div>
+            <p>About</p>
+            <p>Experience</p>
+            <p>Projects</p>
+        </div>
+    )
+}
+
+export default Nav
