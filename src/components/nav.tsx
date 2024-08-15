6a56ff81c13f5982cb4826a8b000b114b219ae4f
enoskyeza <enoskyeza@gmail.com>
2024-08-15 20:51:24 +0300

    modify nav links

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Nav = () => {
     return (
-        <div>
-            <p>About</p>
-            <p>Experience</p>
-            <p>Projects</p>
+        <div className="flex flex-col">
+            <a className="nav-link-custom">About</a>
+            <a className="nav-link-custom">Experience</a>
+            <a className="nav-link-custom">Projects</a>
         </div>
     )
 }
